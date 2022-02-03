31eab4ca3b70c662bcfa3aca5affc96e642f9c69
benvial <b.vial@qmul.ac.uk>
2022-02-03 18:35:17 +0000

    first commit

---
+++ eigenvalues.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Benjamin Vial
+# License: MIT
+import numpy as np
+import time
+import sys
+from utils import get_info
+
+try:
+    import skcuda.magma as magma
+    HAS_SKCUDA = True
+except:
+    HAS_SKCUDA = False
+import scipy.linalg
+
+np.random.seed(1234)
+
+from scipy.linalg import eig
+
+typedict = {"s": np.float32, "d": np.float64, "c": np.complex64, "z": np.complex128}
+typedict_ = {v: k for k, v in typedict.items()}
+
+
+def eig_magma(a):
+    """
+    Eigenvalue solver using Magma GPU library (variants of Lapack's geev).
+
+    Note:
+    - not a generalized eigenvalue solver
+    """
+    if not useMagma:
+        cpu_time = time.time()
+        out = scipy.linalg.eig(a)
+        cpu_time = time.time() - cpu_time
+        if verbose:
+            print("Time for eig: ", cpu_time)
+        return out
+    else:
+        if len(a.shape) != 2:
+            raise ValueError("M needs to be a rank 2 square array for eig.")
+
+        magma.magma_init()
+
+        dtype = type(a[0, 0])
+        t = typedict_[dtype]
+        N = a.shape[0]
+
+        # Set up output buffers:
+        if t in ["s", "d"]:
+            wr = np.zeros((N,), dtype)  # eigenvalues
+            wi = np.zeros((N,), dtype)  # eigenvalues
+        elif t in ["c", "z"]:
+            w = np.zeros((N,), dtype)  # eigenvalues
+
+        vl = np.zeros((1, 1), dtype)
+        jobvl = "N"
+        vr = np.zeros((N, N), dtype)
+        jobvr = "V"
+
+        # Set up workspace:
+        if t == "s":
+            nb = magma.magma_get_sgeqrf_nb(N, N)
+        if t == "d":
+            nb = magma.magma_get_dgeqrf_nb(N, N)
+        if t == "c":
+            nb = magma.magma_get_cgeqrf_nb(N, N)
+        if t == "z":
+            nb = magma.magma_get_zgeqrf_nb(N, N)
+
+        lwork = N * (1 + 2 * nb)
+        work = np.zeros((lwork,), dtype)
+        if t in ["c", "z"]:
+            rwork = np.zeros((2 * N,), dtype)
+
+        # Compute:
+        gpu_time = time.time()
+        if t == "s":
+            status = magma.magma_sgeev(
+                jobvl,
+                jobvr,
+                N,
+                a.ctypes.data,
+                N,
+                wr.ctypes.data,
+                wi.ctypes.data,
+                vl.ctypes.data,
+                N,
+                vr.ctypes.data,
+                N,
+                work.ctypes.data,
+                lwork,
+            )
+        if t == "d":
+            status = magma.magma_dgeev(
+                jobvl,
+                jobvr,
+                N,
+                a.ctypes.data,
+                N,
+                wr.ctypes.data,
+                wi.ctypes.data,
+                vl.ctypes.data,
+                N,
+                vr.ctypes.data,
+                N,
+                work.ctypes.data,
+                lwork,
+            )
+        if t == "c":
+            status = magma.magma_cgeev(
+                jobvl,
+                jobvr,
+                N,
+                a.ctypes.data,
+                N,
+                w.ctypes.data,
+                vl.ctypes.data,
+                N,
+                vr.ctypes.data,
+                N,
+                work.ctypes.data,
+                lwork,
+                rwork.ctypes.data,
+            )
+        if t == "z":
+            status = magma.magma_zgeev(
+                jobvl,
+                jobvr,
+                N,
+                a.ctypes.data,
+                N,
+                w.ctypes.data,
+                vl.ctypes.data,
+                N,
+                vr.ctypes.data,
+                N,
+                work.ctypes.data,
+                lwork,
+                rwork.ctypes.data,
+            )
+
+        if t in ["s", "d"]:
+            w_gpu = wr + 1j * wi
+        else:
+            w_gpu = w
+
+        magma.magma_finalize()
+
+        return w_gpu, vr
+
+
+if __name__ == "__main__":
+    # N = int(sys.argv[1])
+    # useMagma = bool(int(sys.argv[2]))
+    
+    ## just to init
+    matrix = np.random.random((3, 3))
+    W, V = eig(matrix)
+    if HAS_SKCUDA:
+        W, V = eig_magma(matrix)
+
+    matsize = [250, 500, 1000, 2000, 4000, 8000]
+    benchmark_timing = dict(magma=[], scipy=[])
+
+    benchmark_info = dict(magma=[], scipy=[])
+    
+    cases = ["scipy"]
+    if HAS_SKCUDA:
+        cases += ["magma"]
+
+    for N in matsize:
+        print("\n")
+        print("#"*40)
+        print("matrix size = ", N)
+        matrix = np.random.random((N, N))
+        for case in cases:
+            print(">> case: ", case)
+
+            elapsed_time = time.time()
+            if case == "magma":
+                W, V = eig_magma(matrix)
+            else:
+                W, V = eig(matrix)
+            elapsed_time = time.time() - elapsed_time
+            print("   elapsed time = ", elapsed_time)
+            info = get_info(verbose=True)
+            benchmark_info[case].append(info)
+
+    np.savez(
+        "benchmark.npz",
+        matsize=matsize,
+        benchmark_timing=benchmark_timing,
+        benchmark_info=benchmark_info,
+    )
+    
---
+++ utils.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Benjamin Vial
+# License: MIT
+
+
+
+import psutil
+import platform
+import cpuinfo
+import GPUtil
+
+# Let's make a function that converts a large number of bytes into a scaled format (e.g in kilo, mega, Giga, etc.):
+
+def get_size(bytes, suffix="B"):
+    """
+    Scale bytes to its proper format
+    e.g:
+        1253656 => '1.20MB'
+        1253656678 => '1.17GB'
+    """
+    factor = 1024
+    for unit in ["", "K", "M", "G", "T", "P"]:
+        if bytes < factor:
+            return f"{bytes:.2f}{unit}{suffix}"
+        bytes /= factor
+
+
+
+
+def get_info(verbose=False):
+    # System Information
+    # 
+    info = dict()
+    uname = platform.uname()
+    cpu_info = cpuinfo.get_cpu_info()
+    
+    info["uname"] = uname
+    info["cpu_info"] = cpu_info
+
+        
+    # CPU Information
+    # 
+    # Let's get some CPU information, such as the total number of cores, usage, etc:
+
+    # number of cores
+    cores = dict(physical=psutil.cpu_count(logical=False),total=psutil.cpu_count(logical=True))
+    
+    # CPU frequencies
+    cpufreq = psutil.cpu_freq()
+    cores["cpufreq"]=cpufreq
+    # CPU usage
+    percentages=[]
+    for i, percentage in enumerate(psutil.cpu_percent(percpu=True, interval=0.1)):
+        percentages.append(percentage)
+    
+    cores["individual usage"] = percentages
+    cores["total usage"] = psutil.cpu_percent()
+    
+    info["cores"] = cores
+
+    # psutil's cpu_count() function returns number of cores, whereas cpu_freq() function returns CPU frequency as a namedtuple including current, min, and max frequency expressed in Mhz, you can set percpu=True to get per CPU frequency.
+    # 
+    # cpu_percent() method returns a float representing the current CPU utilization as a percentage, setting interval to 1 (seconds) will compare system CPU times elapsed before and after a second, we set percpu to True in order to get CPU usage of each core.
+    # Memory Usage
+
+    # Memory Information
+    # get the memory details
+    svmem = psutil.virtual_memory()
+    info["svmem"] = svmem
+    
+    
+    try:
+        gpus = GPUtil.getGPUs()
+    except:
+        gpus = []
+        
+    info["gpu"] = gpus
+
+    
+    
+    if verbose:
+        
+
+        print("="*33, "System Information", "="*33)
+        print(f"System: {uname.system}")
+        print(f"Node Name: {uname.node}")
+        print(f"Release: {uname.release}")
+        print(f"Version: {uname.version}")
+        print(f"Machine: {uname.machine}")
+        print(f"Processor: {cpu_info['brand_raw']}")
+        print(f"Python: {cpu_info['python_version']}")
+
+        # let's print CPU information
+        print("="*33, "CPU Info", "="*33)
+        print("Physical cores:",cores["physical"])
+        print("Total cores:",cores["total"])
+        print(f"Max Frequency: {cpufreq.max:.2f}Mhz")
+        print(f"Min Frequency: {cpufreq.min:.2f}Mhz")
+        print(f"Current Frequency: {cpufreq.current:.2f}Mhz")
+        
+        print("CPU Usage Per Core:")
+        for i, percentage in enumerate(percentages):
+            print(f"Core {i}: {percentage}%")
+        print(f"Total CPU Usage: {cores['total usage']}%")
+        
+        print("="*33, "Memory Information", "="*33)
+        print(f"Total: {get_size(svmem.total)}")
+        print(f"Available: {get_size(svmem.available)}")
+        print(f"Used: {get_size(svmem.used)}")
+        print(f"Percentage: {svmem.percent}%")
+        
+
+        print("="*33, "GPU Information", "="*33)
+
+        for gpu in gpus:
+
+            print(f"ID: {gpu.id}")
+            print(f"Name: {gpu.name}")
+            print(f"Load: {gpu.load*100}%")
+            print(f"free memory: {gpu.memoryFree}MB")
+            print(f"used memory: {gpu.memoryUsed}MB")
+            print(f"total memory: {gpu.memoryTotal}MB")
+            print(f"temperature: {gpu.temperature} °C")
+            print(f"uuid: {gpu.uuid}")
+
+    return info
+
+
+info = get_info(verbose=True)
